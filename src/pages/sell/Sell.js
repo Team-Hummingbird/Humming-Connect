074c1d94261cc9a738aa8fa1ea116c6504d8ea5a
김지호 <157683506+jiho1998@users.noreply.github.com>
2024-03-26 09:09:01 +0000

    first sell commit

--- src/pages/sell/Sell.js
+++ src/pages/sell/Sell.js
@@ -1,106 +1,218 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Sell.module.css';
+import React, { useEffect, useState } from 'react';
 
 
 
 function Sell () {
+
+
+    const [isInfoAutoFilled, setIsInfoAutoFilled] = useState(false);
+    const [name, setName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [artistName, setArtistName] = useState('');
+    const [title, setTitle] = useState('');
+    const [year, setYear] = useState('');
+    const [materials, setMaterials] = useState('');
+    const [detail, setDetail] = useState('');
+    const [image, setImage] = useState(null);
+    const [previewUrl, setPreviewUrl] = useState('');
+
+    const handleCheckboxChange = () => {
+        setIsInfoAutoFilled(!isInfoAutoFilled);
+    };
+
+    useEffect(() => {
+            if (isInfoAutoFilled) {
+                setName('허밍커넥트');
+                setPhoneNumber('02-1234-5678');
+                setEmail('hummingConnect@hummingConnect.com');
+            } else {
+                setName('');
+                setPhoneNumber('');
+                setEmail('');
+            }
+        }, [isInfoAutoFilled]);
+
+        useEffect(() => {
+            if (!image) {
+                setPreviewUrl('');
+                return;
+            }
+            const fileReader = new FileReader();
+            fileReader.onload = () => {
+                setPreviewUrl(fileReader.result);
+            };
+            fileReader.readAsDataURL(image);
+        }, [image]);
+    
+        const handlerImageChange = (e) => {
+            if (e.target.files && e.target.files.length > 0) {
+                setImage(e.target.files[0]);
+            }
+        };
+    
+    const isFormFilled = name && phoneNumber && email && artistName && title && year && materials && image;
+
+
+
     return (
         <>
         <div className={styles.sell}>판매 신청</div>
-        <div className={styles.sellInfo}>판매자 정보</div>
-        <div className={styles.sellInfoText1}>판매를 원하는 회원님의 정보를 입력해주세요.</div>
-        <div className={styles.sellInfoText2}>입력한 정보는 판매신청 제출용으로만 활용됩니다.</div>
 
-    <div className={styles.myInfoCheck}>내 정보 입력</div>
-        <div className={styles.checkbox}>
-            <input type="checkbox"/>
-        </div>
-        <div className={styles.sellInfoText3}><span class="red-star">* </span>필수입력항목
-        </div>
+
+
+    <div className={styles.infoContainer}>
+        <div className={styles.sellInfo}>판매자 정보</div>
+        <input
+                type="checkbox"
+                checked={isInfoAutoFilled}
+                onChange={handleCheckboxChange}
+                className={styles.infoCheckbox}
+            />
+        <div className={styles.myInfoCheck}>내 정보 입력</div>
+    </div>
+
+    <div className={styles.sellInfoText1}>판매를 원하는 회원님의 정보를 입력해주세요.</div>
+    <div className={styles.sellInfoText2}>입력한 정보는 판매신청 제출용으로만 활용됩니다.</div>
+
+
+    <div className={styles.sellInfoText3}>
+        <span className={styles.redStar}>* </span>필수입력항목
+    </div>
+
     <div className={styles.myInfoCheck1}>
-        <div className={styles.nameBlockInner}></div>
-        <div className={styles.nameBlock}>
-            <span>이름 Name </span>
-            <span className={styles.redStar}>*</span>
-            <span> 예시)허밍커넥트</span>
+        <div className={styles.nameBlockInner}>
+            <div className={styles.nameBlock}>
+                <input type="text" value={name} onChange={(e) => 
+                    setName(e.target.value)} 
+                className={styles.inputField} 
+                placeholder='이름 Name * 예시)허밍커넥트'/>
+            </div>
         </div>
     </div>
     <div className={styles.myInfoCheck2}>
-        <div className={styles.phoneBlockInner}></div>
-        <div className={styles.phoneBlock}>
-            <span>전화번호 Phone Number </span>
-            <span className={styles.redStar}>*</span>
-            <span> (010-1234-5678) </span>
+        <div className={styles.phoneBlockInner}>
+            <div className={styles.phoneBlock}>
+                <input type="text" value={phoneNumber} onChange={(e) => 
+                    setPhoneNumber(e.target.value)} 
+                className={styles.inputField} 
+                placeholder='전화번호 Phone Number * (010-1234-5678) '/>
+            </div>
         </div>
     </div>
     <div className={styles.myInfoCheck3}>
-        <div className={styles.emailBlockInner}></div>
-        <div className={styles.emailBlock}>
-            <span>이메일 Email </span>
-            <span className={styles.redStar}>*</span>
-            <span> 예시)hummingConnect@hummingConnect.com</span>
+        <div className={styles.emailBlockInner}>
+            <div className={styles.emailBlock}>
+            <input type="text" value={email} onChange={(e) => 
+                setEmail(e.target.value)} 
+            className={styles.inputField} 
+            placeholder='이메일 Email * 예시)hummingConnect@hummingConnect.com'/>
+            </div>
         </div>
     </div>
     <div className={styles.artInfo}>작품 정보</div>
     <div className={styles.artInfoText1}>판매를 원하는 작품의 정보를 입력해주세요.</div>
-    <div className={styles.artInfoText2}>입력한 정보는 전문가의 감정평가에 사용되므로 정확한 정보를 입력해주세요.
-    </div>
-    <div className={styles.artInfoText3}><span class={styles.redStar}>※ 불분명한 정보를 입력한 경우, 판매신청이 보류되거나 거절될 수 있습니다. </span>
-    </div>
-    <div className={styles.artInfoText4}><span class={styles.redStar}>* </span>필수입력항목
-    </div>
+    <div className={styles.artInfoText2}>입력한 정보는 전문가의 감정평가에 사용되므로 정확한 정보를 입력해주세요.</div>
+    <div className={styles.artInfoText3}><span className={styles.redStar}>※ 불분명한 정보를 입력한 경우, 판매신청이 보류되거나 거절될 수 있습니다. </span></div>
+    <div className={styles.artInfoText4}><span className={styles.redStar}>* </span>필수입력항목</div>
+
     <div className={styles.artInfoCheck1}>
-        <div className={styles.artistBlockInner}></div>
-        <div className={styles.artistBlock}>
-            <span>작가명 Artist </span>
-            <span className={styles.redStar3}>*</span>
+        <div className={styles.artistBlockInner}>
+            <div className={styles.artistBlock}>
+            <input type="text" value={artistName} onChange={(e) => 
+                setArtistName(e.target.value)} 
+            className={styles.inputField} 
+            placeholder='작가명 Artist * '/>
+            </div>
         </div>
     </div>
+
     <div className={styles.artInfoCheck2}>
-        <div className={styles.titleBlockInner}></div>
-        <div className={styles.titleBlock}>
-            <span>작품명 Title</span>
-            <span className={styles.redStar4}>*</span>
+        <div className={styles.titleBlockInner}>
+            <div className={styles.titleBlock}>
+            <input type="text" value={title} onChange={(e) => 
+                setTitle(e.target.value)} 
+            className={styles.inputField} 
+            placeholder='작품명 Title *'/>
+            </div>
         </div>
     </div>
+
     <div className={styles.artInfoCheck3}>
-        <div className={styles.yearBlockInner}></div>
-        <div className={styles.yearBlock}>
-            <span>작품 제작 연도 Year</span>
-            <span className={styles.redStar5}>*</span>
+        <div className={styles.yearBlockInner}>
+            <div className={styles.yearBlock}>
+            <input type="text" value={year} onChange={(e) => 
+                setYear(e.target.value)} 
+            className={styles.inputField} 
+            placeholder='작품 제작 연도 Year *'/>
+            </div>
         </div>
-        </div>
-    <div className={styles.artInfoCheck4}>
-        <div className={styles.materialsBlockInner}></div>
-        <div className={styles.materialsBlock}>
-            <span>작품 소재 Materials</span>
-            <span className={styles.redStar5}>*</span>
-        </div>
-        </div>
-        <div className={styles.artInfoCheck5}>
-            <div className={styles.detailBlockInner}></div>
-            <div className={styles.detailBlock}>
-                <span>상세 설명(선택 입력 사항)</span>
     </div>
+
+    <div class={styles.artInfoCheck4}>
+        <div class={styles.materialsBlockInner}>
+            <div class={styles.materialsBlock}>
+            <input type="text" value={materials} onChange={(e) => 
+                setMaterials(e.target.value)} 
+            className={styles.inputField} 
+            placeholder='작품 소재 Materials *'/>
+            </div>
         </div>
-        <div className={styles.artImg}>작품 이미지</div>
-        <div className={styles.artImgText1}>판매할 작품의 이미지를 등록해주세요.</div>
-        <div className={styles.artImgText2}>신속한 승인절차를 위해 고해상도의 이미지를 업로드해주세요.
+    </div>
+
+    <div className={styles.artInfoCheck5}>
+        <div className={styles.detailBlockInner}>
+            <div className={styles.detailBlock}>
+            <textarea value={detail} onChange={(e) => 
+                setDetail(e.target.value)} 
+            className={styles.inputFieldDetail} 
+            placeholder='상세 설명(선택 입력사항) '/>
+            </div>
         </div>
-        <div className={styles.addImg}>
-            <div className={styles.addImgBlockInner}></div>
+    </div>
+
+    <div className={styles.artImg}>작품 이미지
+    </div>
+
+    <div className={styles.artImgText1}>판매할 작품의 이미지를 등록해주세요.
+    </div>
+
+    <div className={styles.artImgText2}>신속한 승인절차를 위해 고해상도의 이미지를 업로드해주세요.
+    </div>
+
+    <div className={styles.addImg}>
+        <div className={styles.addImgBlockInner}>
             <div className={styles.addImgBlock}>
                 <span>이미지 등록 Add Image</span>
                 <span className={styles.redStar}>*</span>
-                <button class={styles.imageUploadButton}>이미지 등록</button>
                 <br/>
                 <br/>
-                <span className={styles.redStar}>※ 파일형식은 JPG, JPEG, PNG를 지원하며 파일크기는 최대 30MB까지 업로드 가능합니다.</span>
+                <span className={styles.redStar1}>※ 파일형식은 JPG, JPEG, PNG를 지원하며 파일크기는 최대 30MB까지 업로드 가능합니다.</span>
+                <input
+                    type="file"
+                    onChange={handlerImageChange}
+                    accept="image/png, image/jpeg"
+                    style={{ display: 'none' }}
+                    id="image-upload"
+                />
+                <label htmlFor="image-upload" className={styles.imageUploadButton}>
+                    이미지 등록
+                </label>
+            </div>
         </div>
-        <div className={styles.imgRegister}></div>
-        <button className={styles.imgRegister}>판매 신청 등록</button>
-    </div>
-    </>
+
+        {previewUrl && (
+            <div className={styles.imagePreviewContainer}>
+                <img src={previewUrl} alt="Preview" className={styles.imagePreview} />
+            </div>
+)}
+</div>
+    <button
+        className={`${styles.imgRegister} ${isFormFilled ? styles.imgRegisterActive : ''}`}
+            disabled={!isFormFilled}>판매 신청 등록</button>
+        </>
     );
 }
 
---
+++ src/pages/sell/SellDone.js
@@ -0,0 +1,16 @@
+import styles from './Sell.module.css';
+import React, { useEffect, useState } from 'react';
+
+
+function SellDone() {
+
+    <div className={styles.sellDoneText1}>
+        <span className={styles.Done1}>판매신청 </span>
+        <span className={styles.Done2}>이 </span>
+        <span className={styles.Done3}>완료 </span>
+        <span className={styles.Done4}>되었습니다. </span>
+    </div>
+
+}
+
+export default SellDone;
