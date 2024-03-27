833731927015960955099de886e89a7a7be7d919
kooyesung <jh8ckr@gmail.com>
2024-03-27 18:00:06 +0900

    commit

--- src/components/HummingConnect.js
+++ src/components/HummingConnect.js
@@ -1,6 +1,8 @@
+import Hc from "./HummingConnect.module.css"
+
 function HummingConnect() {
     return (
-        <div>
+        <div className={Hc.connectBox}>
             <img 
             alt="채팅창"
             src="img/Group 144.png"
