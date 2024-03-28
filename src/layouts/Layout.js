d036b1093196d994023532eed032b27ee8737d87
NJH <dldks1212@gmail.com>
2024-03-29 00:04:51 +0900

    add scrollToTop button component

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -3,11 +3,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import TopBtn from '../components/TopBtn';
 
 export default function Layout(){
   
   return(
     <>
+      <TopBtn/>
       <Header/>      
       <Outlet/>
       <Footer/>
